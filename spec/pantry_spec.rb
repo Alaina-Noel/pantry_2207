80d30ca8a4ea13f67a9691e738bf5d55321af4dc
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-02 09:43:16 -0600

    Fin It 1

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe do
     expect(pantry).to be_instance_of(Pantry)
   end
 
-  xit 'has an empty hash as a stock by default' do
+  it 'has an empty hash as a stock by default' do
     pantry = Pantry.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
@@ -15,7 +15,7 @@ RSpec.describe do
     expect(pantry.stock).to eq({})
   end
 
-  xit 'has default 0 on a specific ingredient before its been restocked' do
+  it 'has default 0 on a specific ingredient before its been restocked' do
     pantry = Pantry.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
@@ -24,7 +24,7 @@ RSpec.describe do
     expect(pantry.stock_check(ingredient2)).to eq(0)
   end
 
-  xit 'can restock a specific ingredient and quantity' do
+  it 'can restock a specific ingredient and quantity' do
     pantry = Pantry.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
@@ -33,7 +33,7 @@ RSpec.describe do
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
     expect(pantry.stock_check(ingredient1)).to eq(15)
-    pantry.restock(ingredient2, 10)
+    pantry.restock(ingredient2, 7)
     expect(pantry.stock_check(ingredient2)).to eq(7)
   end
 
